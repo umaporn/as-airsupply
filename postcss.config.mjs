ea308cda609ee1aef2735b474092cd41e270f414
Umaporn D <umaporn@app-bit.co.th>
2024-12-26 10:28:55 +0700

    Updated post css config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,22 +1,7 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
+    'tailwindcss/nesting': {},
     tailwindcss: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
-    ...(process.env.NODE_ENV === 'production'
-      ? {
-          '@fullhuman/postcss-purgecss': {
-            content: [
-              './pages/**/*.{js,ts,jsx,tsx}',
-              './components/**/*.{js,ts,jsx,tsx}',
-              './app/**/*.{js,ts,jsx,tsx}',
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        }
-      : {}),
+    autoprefixer: {},
   },
 };
-
-export default config;
